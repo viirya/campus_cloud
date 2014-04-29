04fe03e9a36e609fe4be990c00733642a3d8b4e5
Liang-Chi Hsieh <viirya@gmail.com>
2014-04-29 18:13:41 +0800

    simple python script to process image download.

---
+++ download_image.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import urllib
+import os
+import re
+import sys
+
+def crawl_image_from_url(url, filename):
+
+    #print("crawling " + url + " ...")
+    try: 
+        f = open("./images/" + filename, 'wb')
+        f.write(urllib.urlopen(url).read())
+        f.close()
+        #os.system("/opt/hadoop/hadoop/bin/hdfs dfs -put /tmp/" + filename + " /user/hduser/images/" + filename)
+    except Exception as e:
+        return "Error: " + url + " " + e.strerror
+
+    return url
+
+def regex_image(url):
+    m = re.search('.*\/(.*)', url)
+    return m.group(1)
+
+for line in sys.stdin:
+    line = line.strip()
+    filename = regex_image(line)
+    print crawl_image_from_url(line, filename)
+
+
