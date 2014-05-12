800389d19c0facc47ec060176e5614459ccf8ef1
Liang-Chi Hsieh <viirya@gmail.com>
2014-05-13 02:15:12 +0800

    calculate std of created time of tweets.

--- parse_hashtag.py
+++ parse_hashtag.py
@@ -4,16 +4,27 @@ import urllib
 import os
 import re
 import sys
+from dateutil import parser
 
-def parse_hashtags_from_tweet(tweet):
+def parse_hashtags_from_tweet(tweet, created_at):
+
+    time = parse_datetime_from_tweet(created_at)
 
     matches = re.findall('(#(?![#\s])(.(?!\s))*\S)', tweet)
     for match in matches:
-        print match[0]
+        print match[0] + "\t" + str(time)
 
+def parse_datetime_from_tweet(created_at):
+
+    duration = parser.parse(created_at) - parser.parse("Thu Jan 1 00:00:00 +0000 2013")
+    return duration.total_seconds()
 
 for line in sys.stdin:
     line = line.strip()
-    parse_hashtags_from_tweet(line)
+    fields = line.split('\t')
+    created_at = fields[len(fields) - 1]
+    tweet = " ".join(fields[0:len(fields) - 1])    
+    if len(fields) >= 2:
+        parse_hashtags_from_tweet(tweet, created_at)
 
 
